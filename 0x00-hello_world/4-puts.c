c83d82cad15e41b4e402b78b5157183c2274b37e
M17ge <ashtonjonah77@gmail.com>
2023-08-14 06:26:36 -0700

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main - mian entry point
+  * return always zero
+  * return : always 0 (success)
+*/
+#include <stdlib.h>
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return  (0);
+}
